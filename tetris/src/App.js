9ecce4183ce05b533fbd093a80f66a686d04fbb9
Tom Bauer <tbauer1898@gmail.com>
2020-11-23 16:42:54 -0600

    control layout added

--- tetris/src/App.js
+++ tetris/src/App.js
@@ -5,7 +5,7 @@ import GridSquare from './components/GridSquare';
 import GridBoard from './components/GridBoard';
 import NextBlock from './components/NextBlock';
 import ScoreBoard from './components/ScoreBoard'
-
+import Controls from './components/Controls'
 
 function App() {
   return (
@@ -16,6 +16,7 @@ function App() {
       <GridBoard />
       <NextBlock />
       <ScoreBoard />
+      <Controls />
     </div>
   );
 }
