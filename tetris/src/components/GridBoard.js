346974dc25a47573098f32dfbad9958a83c02af4
Tom Bauer <tbauer1898@gmail.com>
2020-11-24 16:59:41 -0600

    Grid board is connected

--- tetris/src/components/GridBoard.js
+++ tetris/src/components/GridBoard.js
@@ -1,25 +1,51 @@
 import React from 'react';
+import { useSelector } from 'react-redux'
+import { shapes } from '../utils'
+
 import GridSquare from './GridSquare.js'
 
 function GridBoard(props) {
-
+    const game = useSelector((state) => state.game)
+    const { grid, shape, rotation, x, y, isRunning, speed} = game
     // generates an array of 18 rows, each containing 10 GridSquares
     // grid will be 10 x 18
 
-    const grid = []
-    for(let row = 0; row < 18; row++) {
-        grid.push([])
-        for(let col = 0; col < 10; col++) {
-            grid[row].push(<GridSquare key={`${col}${row}`} color ="0" />)
-        }
-    }
+    const block = shapes[shape][rotation]
+    const blockColor = shape
+    // map rows
+    const gridSquares = grid.map((rowArray, row) => {
+        // map columns
+        return rowArray.map((square, col) => {
+            // Find block x and y on the shape grid
+            // By subtracting the x and y from the col and the row we get the position of the upper left corner of the block array
+            // as if it was superimposed on the main grid
+            const blockX = col - x
+            const blockY = row - y
+            let color = square
+            // Map current falling blocks to grid
+            // For any squares that fall on the grid we need to look at the block array and see if there is a 1 in this case
+            // we use the block color
+            if (blockX >= 0 && blockX < block.length && blockY >= 0 && blockY < block.length) {
+                color = block[blockY][blockX] === 0 ? color : blockColor
+            }
+            // generate a unique key for every block
+            const k = row * grid[0].length + col;
+            // generate a grid square
+            return <GridSquare key={k} color={color} />
+        })
+    })
 
     return (
         <div className='grid-board'> 
-            {grid}
+            {gridSquares}
         </div>
     )
 
-};
+    
+    };
+
+    
+
+
 
 export default GridBoard
